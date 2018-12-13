6b4ce6dcccb07077c48db743a069200203dbb5ec
Brendan Goodenough <brendan@goodenough.nz>
2018-12-13 21:18:01 +1300

    Add initial app structure with Tabs component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Tabs from './components/Tabs'
+import './App.css'
+
+class App extends Component {
+  public render() {
+    return (
+      <div className="app">
+        <Tabs />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+const Tabs: React.StatelessComponent<{}> = () => {
+  return <div className="tabs" />
+}
+
+export default Tabs
