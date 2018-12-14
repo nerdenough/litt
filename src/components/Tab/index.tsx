68698d492ded509bcace35743c99b20458844cb5
Brendan Goodenough <brendan@goodenough.nz>
2018-12-14 22:41:12 +1300

    Highlight active tab

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 import './styles.css'
 
@@ -12,7 +13,11 @@ interface Props {
 const Tab: React.StatelessComponent<Props> = props => {
   return (
     <Link to={props.path}>
-      <div className="tab">
+      <div
+        className={classNames('tab', {
+          active: props.path === window.location.pathname,
+        })}
+      >
         <FontAwesomeIcon icon={props.icon} />
       </div>
     </Link>
